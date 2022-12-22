6b9ea6fe58006338229f80dd937a81b8b3927f06
Terry Lee Chen <terryleechen@gmail.com>
2022-12-22 15:03:22 -0700

    most methods are done

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,22 @@ mod setup;
 mod database;
 
 fn main() {
-    // let mut file = match File::open(&path) {
-    //     Err(why) => panic!("couldn't open {}: {}", display, why),
-    //     Ok(file) => file,
-    // };
-
-    // let something = match setup::read_file::read_lines("setup/simple.csv") {
-    //     Err(why) => panic!("couldn't open file: {}", why),
-    //     Ok(something) => something,
-    // };
-    // println!("something: {:?}", something);
-    
+    // create a new database
     let mut db = database::db::Database::new();
     
-    setup::read_file::read_lines("setup/simple.csv",&mut db);
+    //setup::read_file::read_lines("setup/simple.csv",&mut db);
+    setup::read_file::read_lines("setup/full.csv",&mut db);
     
    
-    println!(" db len {}", &db.get_tables_length());
+    println!("There are {} picnic tables!", &db.get_tables_length());
     //println!(" id {}", &db.get_table(0).get_id());
     //println!("{}", &db.count_entries("Square Picnic Table".to_string()));
     db.edit_table_entry(10846, "Square Picnic Table".to_string(), "new_value".to_string());
-    println!(" id {}", &db.get_table(0).get_table_type());
+    println!("The id {} table is a {}", &db.get_table(0).get_id(), &db.get_table(0).get_table_type());
+    println!("There are {} Square Picnic Table", db.count_entries("Square Picnic Table".to_string()));
+    println!("There are {} structural material types", db.get_structural_material_table_length());
+    println!("There are {} surface material types", db.get_surface_material_table_length());
+    println!("There are {} table types", db.get_table_type_table_length());
     db.report_by_ward();
     db.report_by_neighbourhood();
     db.list_table_type();
